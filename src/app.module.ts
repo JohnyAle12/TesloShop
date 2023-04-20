867a38e089031195231afeea4c6276f508e210ed
Johny Prieto <johnyprieto001@gmail.com>
2023-04-20 12:09:25 -0500

    add typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DATABASE,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true, //upload the entities automatic while we are creating it
+      synchronize: false //allow to synchronize the changes inside the entities, for example columns or data types
+    })
+  ],
   controllers: [],
   providers: [],
 })
