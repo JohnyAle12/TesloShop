1cd10a8a7b950257e10353cb49b41f54c93276cf
Johny Prieto <johnyprieto001@gmail.com>
2023-04-26 20:01:03 -0500

    serve image files on controlled way

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helpers';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/fileNamer.helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
@@ -22,5 +23,17 @@ export class FilesController {
     @UploadedFile() file: Express.Multer.File
   ) {
     return this.filesService.upload(file);
+  }
+
+  @Get('product/:image')
+  findProductImage(
+    @Res() response: Response,
+    @Param('image') image: string
+  ){
+    //using @Res decorator we are using the express responses and we stop to use the nest framework
+    //it can be dangerous because we break the nest life cycle, and function such as interceptors or restrictions from nest wont work
+    const file = this.filesService.getImage(image)
+    response.sendFile(file)
+    // return { file }
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,9 +1,24 @@
+import { join } from 'path';
+import { existsSync } from 'fs';
 import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FilesService {
   upload(file: Express.Multer.File) {
     if(!file) throw new BadRequestException('Make sure that you select an image')
-    return file;
+
+
+    const secureUrl = file.filename
+
+    return {
+      secureUrl
+    };
+  }
+
+  getImage(image: string){
+    const path = join( __dirname, '../../static/uploads', image )
+    if(!existsSync(path)) throw new BadRequestException(`Image ${image} not found`)
+
+    return path
   }
 }
