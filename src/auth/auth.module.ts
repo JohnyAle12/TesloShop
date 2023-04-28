68a1f32e1ce1aa4784c24c326f40f203aef50184
Johny Prieto <johnyprieto001@gmail.com>
2023-04-27 21:51:15 -0500

    add async module to config our jwt tokens signed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -10,7 +13,28 @@ import { User } from './entities/user.entity';
   imports: [
     TypeOrmModule.forFeature([
       User
-    ])
+    ]),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    // using async modules, we can import modules and inject dependencies, here we used config service to get .env
+    //using this module to sing our jwt tokens
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2h'
+        }
+      })
+    })
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ],
   exports: [ TypeOrmModule ]
 })
