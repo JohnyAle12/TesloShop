869293ca600bd1b18ca166fed1cd2fe379c073a6
Johny Prieto <johnyprieto001@gmail.com>
2023-05-03 13:32:14 -0500

    create relationship product and user

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProducImage } from "./product-image.entity";
+import { User } from "src/auth/entities/user.entity";
 
 @Entity({ name: 'products' })
 export class Product {
@@ -47,6 +48,8 @@ export class Product {
     })
     tags: string[]
 
+    //eager: true -> allow that each query we can retrieve this property inside the query
+
     @OneToMany(
         () => ProducImage,
         productImage => productImage.product,
@@ -56,6 +59,13 @@ export class Product {
         }
     )
     images?: ProducImage[]
+
+    @ManyToOne(
+        () => User,
+        user => user.product,
+        { eager:true }
+    )
+    user: User
 
     @BeforeInsert()
     checkSlugInsert(){
