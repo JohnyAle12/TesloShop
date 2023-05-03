c33ae9e3d031e294e0e222040242e24a1a13015e
Johny Prieto <johnyprieto001@gmail.com>
2023-05-03 11:36:13 -0500

    create a custom decorators

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query, UseGuards, Res, Headers } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/auth/entities/user.entity';
+import { RawHeaders } from 'src/common/decorators/raw-headers.decorator';
+import { IncomingHttpHeaders } from 'http';
 
 @Controller('products')
 export class ProductsController {
@@ -16,8 +20,32 @@ export class ProductsController {
 
   @Get()
   @UseGuards(AuthGuard())
-  findAll( @Query() paginationDto:PaginationDto) {
+  findAll(
+    @Query() paginationDto:PaginationDto,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: User,
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders
+  ) {
+
+    console.log({
+      user,
+      userEmail,
+      rawHeaders,
+      headers
+    });
+    
     return this.productsService.findAll(paginationDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  privateAccess(
+    @GetUser() user: User,
+  ) {
+    return {
+      user
+    };
   }
 
   @Get(':term')
@@ -36,4 +64,5 @@ export class ProductsController {
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
+
 }
