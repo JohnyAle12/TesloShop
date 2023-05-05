255793ac6712c9918ac3ada8cf7479a5030cc1a1
Johny Prieto <johnyprieto001@gmail.com>
2023-05-04 20:30:31 -0500

    emit event to show all users connected

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,19 +1,24 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() private wss: Server
+
   constructor(
     private readonly messagesService: MessagesService
   ) {}
 
   handleConnection(client: Socket){
     this.messagesService.registerClient(client)
-    console.log('clientes conectados', this.messagesService.getConnectedClients());
+    this.wss.emit('updated-clients', this.messagesService.getConnectedClients())
+    console.log('clientes conectados', this.messagesService.getConnectedClients().length);
   }
 
   handleDisconnect(client: Socket){
     this.messagesService.removeClient(client.id)
+    this.wss.emit('updated-clients', this.messagesService.getConnectedClients())
   }
 }
